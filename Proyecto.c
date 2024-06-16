310d8ef42f00a4ea53a03ff51ef1207aca53d078
JhonatanHZ <jhonatanhomsany@gmail.com>
2024-06-16 11:46:06 -0400

    Implementados los operadores AND y OR

--- Proyecto.c
+++ Proyecto.c
@@ -6,23 +6,47 @@
 #include <string.h>
 #include <unistd.h>
 
-bool inputValidation(char program[], char arguments[], int size){
+bool inputValidation(char program[], char *arguments[], int size){
     if((int) program[0] < 65 || (int) program[0] > 122){
         return 0;
     }
 
+    
+
     return 1;
 }
 
-void executeCommand(char *command, char *argumentArray[]){
+int executeCommand(char *command, char *argumentArray[]){
 
-    if(execvp(command, argumentArray) == -1){
-        exit(0);
+    return execvp(command, argumentArray);
+}
+
+int executeProgram(char *arguments[], int size){
+    if(inputValidation(arguments[0], arguments, size)){
+        pid_t child = fork();
+        if(child == 0){
+            if(size == 1){
+                return executeCommand(arguments[0], NULL);
+            }
+            else{
+                return executeCommand(arguments[0], arguments);
+            }
+        }
+        else{
+            wait(NULL);
+        }
+    }
+    else{
+        printf("ERROR: Invalid command. \n");
+        return -1;
     }
 }
 
 void readLine(char input[]){ 
     char *argumentArray[20];
+    char *argumentsFirstProgram[20];
+    char *argumentsSecondProgram[20];
+    int positionOfOperator = 0;
     int argumentIndex = 0;
     char *argument = strtok(input, " \n");
     argumentArray[argumentIndex] = argument;
@@ -36,22 +60,35 @@ void readLine(char input[]){
         }
     }
 
-    if(inputValidation(argumentArray[0], argumentArray, argumentIndex)){
-        pid_t child = fork();
-        if(child == 0){
-            if(argumentIndex == 1){
-                executeCommand(argumentArray[0], NULL);
-            }
-            else{
-                executeCommand(argumentArray[0], argumentArray);
-            }
+    for(int i = 0; i < argumentIndex- 1; i++){
+        if(strcmp(argumentArray[i], "||") == 0 || strcmp(argumentArray[i], "&&") == 0){
+            positionOfOperator = i;
+            break;
         }
-        else{
-            wait(NULL);
+        argumentsFirstProgram[i] = argumentArray[i];
+    }
+
+    if(positionOfOperator != 0){
+        for(int i = positionOfOperator + 1, j = 0; i < argumentIndex- 1; i++, j++){
+            argumentsSecondProgram[j] = argumentArray[i];
         }
     }
-    else{
-        printf("ERROR: Invalid command. \n");
+
+    int sizeOfFirstProgram = positionOfOperator;
+    int sizeOfSecondProgram = argumentIndex - positionOfOperator -2;
+    
+    if(positionOfOperator == 0){
+        executeProgram(argumentsFirstProgram, sizeOfFirstProgram);
+    }
+    else if(strcmp(argumentArray[positionOfOperator], "||") == 0){
+        if(executeProgram(argumentsFirstProgram, sizeOfFirstProgram) == -1){
+            executeProgram(argumentsSecondProgram, sizeOfSecondProgram);
+        }
+    }
+    else if(strcmp(argumentArray[positionOfOperator], "&&") == 0){
+        if(executeProgram(argumentsFirstProgram, sizeOfFirstProgram) != -1){
+            executeProgram(argumentsSecondProgram, sizeOfSecondProgram);
+        }
     }
 }
 
